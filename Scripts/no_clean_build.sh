e97a36539bb7a20ecdd651e3984bbe681ef19a8f
Obi Obetta <obettasteph@linux5.cs.uchicago.edu>
2025-11-19 15:24:19 -0600

    Scripts to actually run the damn thing

--- Scripts/no_clean_build.sh
+++ Scripts/no_clean_build.sh
@@ -7,11 +7,9 @@ NINJA=$(which ninja)
 mkdir -p ./cmake-build-release
 $CMAKE -DCMAKE_BUILD_TYPE=Release -DCMAKE_MAKE_PROGRAM=$NINJA -G Ninga -S . -B ./cmake-build-release
 
-$CMAKE --build ./cmake-build-release --target clean -g 4
 $CMAKE --build ./cmake-build-release --target al -j 4
 
 mkdir -p ./cmake-build-debug
 $CMAKE -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=$NINJA -G Ninja -S . -B ./cmake-build-debug
 
-$CMAKE --build ./cmake-build-release --target clean -g 4
 $CMAKE --build ./cmake-build-release --target al -j 4
